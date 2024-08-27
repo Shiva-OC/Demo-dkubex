8ef6d1968ed157804a53bb6ed79b83f89a4a62d2
Shiva kumar <166685858+Shiva-OC@users.noreply.github.com>
2024-08-27 17:57:45 +0530

    Create app_startup_demo1.sh

---
+++ app_startup_demo1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p .streamlit/
+echo "[server]" > .streamlit/config.toml
+echo "baseUrlPath = \"$INGRESS_PREFIX\"" >> .streamlit/config.toml
+# echo "[theme]" >> .streamlit/config.toml
+# echo "base = \"dark\"" >> .streamlit/config.toml
+streamlit run demo.py --server.port=8501 --server.address=0.0.0.0
